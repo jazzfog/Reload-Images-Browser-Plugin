54902bf6a9b23f53c266edf4eb6f1cbd9f925724
Jupiter-Liar <jomaisaficusl@gmail.com>
2024-08-06 20:19:30 -0400

    Add files via upload
    Manifest V3

--- src/bg_script.js
+++ src/bg_script.js
@@ -1,8 +1,13 @@
-chrome.browserAction.onClicked.addListener(function (tab) {
-		
-	var message = {
+chrome.action.onClicked.addListener((tab) => {
+	console.log("Background script: action clicked");
+
+	chrome.tabs.sendMessage(tab.id, {
 		instruction: 'reloadFailedImages'
-	};
-	
-	chrome.tabs.sendMessage(tab.id, message);
+	}, (response) => {
+		if (chrome.runtime.lastError) {
+			console.error("Error in background script:", chrome.runtime.lastError);
+		} else {
+			console.log("Background script: message sent, response:", response);
+		}
+	});
 });
